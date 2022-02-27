37b54ab21e48d487ca5cda5231d0ecd373bddc81
TiszaHedi <vigus1990@gmail.com>
2022-02-27 12:17:45 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const {src, dest, watch} = require('gulp');
+const compileSass = require('gulp-sass')(require('sass'));
+const minifyCss = require('gulp-clean-css');
+const concat = require('gulp-concat');
+
+const bundleSass = () => {
+    return src('./static/scss/**/*.scss')
+    .pipe(compileSass())
+    .pipe(minifyCss())
+    .pipe(concat('bundle.css'))
+    .pipe(dest('./dist/css/'));
+}
+
+const copyStaticFiles = () => {
+    return src('./static/html/*')
+    .pipe(dest('./dist'));
+}
+
+const devWatch = () => {
+    watch('./static/scss/**/*.scss', bundleSass);
+    watch('./static/html/*', copyStaticFiles);
+}
+
+exports.bundleSass = bundleSass;
+exports.copyStaticFiles = copyStaticFiles;
+exports.devWatch = devWatch;
