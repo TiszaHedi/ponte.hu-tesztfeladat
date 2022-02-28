934175ae00abe5bd571a3b70724a054ffe9ecedd
TiszaHedi <vigus1990@gmail.com>
2022-02-28 12:35:55 +0100

    refact gulpfile.js, add js, fix parners + footer styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ const {src, dest, watch} = require('gulp');
 const compileSass = require('gulp-sass')(require('sass'));
 const minifyCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const merge = require('merge-stream');
+
+const bundle = () => {
+    return bundleSass().
+    pipe(copyStaticFiles());
+}
 
 const bundleSass = () => {
     return src('./static/scss/global.scss')
@@ -12,15 +18,25 @@ const bundleSass = () => {
 }
 
 const copyStaticFiles = () => {
-    return src('./static/html/*')
-    .pipe(dest('./dist'));
+    const folders = [
+        {src: 'static/html/*', dest: 'dist'},
+        {src: 'static/assets/*', dest: 'dist/assets'},
+        {src: 'static/js/*', dest: 'dist/js'}
+    ];
+
+    let tasks = folders.map((el) => {
+        return src(el.src)
+        .pipe(dest(el.dest));
+    })
+
+    return merge(tasks);
 }
 
 const devWatch = () => {
     watch('./static/scss/**/*.scss', bundleSass);
-    watch('./static/html/*', copyStaticFiles);
+    watch('./static/html/*', copyStaticFiles)
+    watch('./static/js/*', copyStaticFiles);
 }
 
-exports.bundleSass = bundleSass;
-exports.copyStaticFiles = copyStaticFiles;
+exports.bundle = bundle;
 exports.devWatch = devWatch;
---
+++ static/js/scripts.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function () {
+    const messageInput = document.getElementById('message');
+
+    messageInput.onkeydown = function(e) {
+        let counterInfo = document.querySelector('.contact-form__character-counter');
+        let counter = this.value.length;
+        let maxLength = 200;
+    
+        counterInfo.innerHTML = 'maximium 200 karakter - ' + (maxLength - counter) + ' maradt'
+    }
+})();
+
+(function () {
+    const inputs = document.querySelectorAll('.contact-form__input-field');
+    const inputClass = ' filled';
+
+    for(let i = 0; i < inputs.length; i++) {
+        let elem = document.getElementById(inputs[i].id);
+        inputs[i].addEventListener('keyup', function(e) {
+            let value = e.target.value;
+            console.log(elem.className);
+            elem.className = (value.length > 0) ? elem.className.replace(inputClass, '') + inputClass : elem.className.replace(inputClass, '');
+        });
+    };
+})();
+
